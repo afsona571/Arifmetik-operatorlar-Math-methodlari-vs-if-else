0067b5193f7f24928f8424a03ded8fb340a503fb
afsona571 <afsonaa59@gmail.com>
2024-11-16 17:21:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+alert('biz sizga random sin chiqarmoqchi miz')
+
+let random = Math.floor(Math.random() * 10)
+alert('randomm son ' + random)
+
+let kopaytiry = random  * 2
+let bolu = random / 2
+let qol = bolu / 3 
+confirm("biz random sonni 2 ga kopaytirip va bolmoqchi miz")
+alert('random chiqan   '+ random +' edi \n biz uni   2 ga kopaytirip  ' + kopaytiry + '  soni chiardik, \n va shu soni 2 ga bolib '+ bolu + ' soni chiqardik \n va shu sonni 3 ga bolgandik:'+ qol +' 3 sonni chiaardik ' )
